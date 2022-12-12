a1aac161a293826fcec9bd3053ec2a8f2abaa3a7
Delos-343 <mfachry.handoko@gmail.com>
2022-12-13 00:26:55 +0700

    testing canvas

--- src/components/Ships.jsx
+++ src/components/Ships.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Gunboat } from './canvas'
 
 const Ships = () => {
   return (
@@ -29,16 +30,27 @@ const Ships = () => {
         </div>
         <div id="myTabContent">
             <div className="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                <p className="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                    This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.
+                </p>
             </div>
             <div className="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
-                <p className="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                    This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.
+                </p>
             </div>
             <div className="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="settings" role="tabpanel" aria-labelledby="settings-tab">
-                <p className="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                    This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.
+                </p>
             </div>
             <div className="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="contacts" role="tabpanel" aria-labelledby="contacts-tab">
-                <p className="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                <div className="w-full h-1/5">
+                    {/* <Gunboat /> */}
+                </div>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                    This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.
+                </p>
             </div>
         </div>
     </>
--- src/components/canvas/Morrigan.jsx
+++ src/components/canvas/Morrigan.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
+import useSpline from '@splinetool/r3f-spline'
+import { OrthographicCamera } from '@react-three/drei'
 
-const Morrigan = () => {
+export default function Morrigan({ ...props }) {
+  const { nodes, materials } = useSpline('https://prod.spline.design/DSmmUXjI6EsZXwVl/scene.splinecode')
   return (
-    <div>
-        <canvas id="canvas"></canvas>
-    </div>
+    <>
+      <color attach="background" args={['#0c0c0c']} />
+      <group {...props} dispose={null}>
+        <mesh
+          name="xxx_-_morrigan"
+          geometry={nodes['xxx_-_morrigan'].geometry}
+          material={nodes['xxx_-_morrigan'].material}
+          castShadow
+          receiveShadow
+          scale={1.5}
+        />
+        <mesh
+          name="Text"
+          geometry={nodes.Text.geometry}
+          material={materials['Text Material']}
+          castShadow
+          receiveShadow
+          position={[-234.94, -121, 454]}
+        />
+        <mesh
+          name="Rectangle 2"
+          geometry={nodes['Rectangle 2'].geometry}
+          material={materials['Rectangle 2 Material']}
+          castShadow
+          receiveShadow
+          position={[-231.95, 90, 0]}
+          scale={0.69}
+        />
+        <directionalLight
+          name="Directional Light"
+          castShadow
+          intensity={0.7}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-near={-10000}
+          shadow-camera-far={100000}
+          shadow-camera-left={-500}
+          shadow-camera-right={500}
+          shadow-camera-top={500}
+          shadow-camera-bottom={-500}
+          position={[200, 300, 300]}
+        />
+        <OrthographicCamera
+          name="1"
+          makeDefault={true}
+          far={100000}
+          near={-100000}
+          position={[0, 0, 1000]}
+          rotation={[0, 0, 0]}
+        />
+        <hemisphereLight name="Default Ambient Light" intensity={0.75} color="#eaeaea" />
+      </group>
+    </>
   )
 }
-
-export default Morrigan
