b1d604b041fe680eff85e0c8d84d9b408d1e15e3
Delos-343 <mfachry.handoko@gmail.com>
2022-12-12 18:00:04 +0700

    finished new setup - retrying now

---
+++ src/routes/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Hero, Navbar } from '../components'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+    </div>
+  )
+}
+
+export default HomePage
